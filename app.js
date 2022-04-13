4b95ab42702273bc2e241af675dc8c16191b2685
redfox <76628594+purplehippo911@users.noreply.github.com>
2022-04-13 11:10:41 +0200

    soon finished🎈🎆🎇

--- app.js
+++ app.js
@@ -1,18 +1,24 @@
+const card = document.querySelector('.card');
 const openDialog = document.querySelector('.submit');
 const dialog = document.querySelector('#dialog');
 const buttons = document.querySelectorAll('.button');
-const rateSpan = document.querySelector('.rate');
+const ratediv = document.querySelector('span');
 let number;
 
 buttons.forEach((button) => {
         button.addEventListener('click', () => {
-            number = buttons.innerHTML;
-            console.log(number);
+            number = button.innerHTML;
+            const htmlText = `<span>${number}</span> `;
+            ratediv.innerHTML = htmlText;
+
         });
     });
 
 openDialog.addEventListener('click', () => {
     if (number > 0) {
+        card.classList.add('close');
+        card.classList.add('transition');
         dialog.classList.add('active');
+        dialog.classList.add('transition');
     }
 })
